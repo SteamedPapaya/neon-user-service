3135b56e6b5347d460295b7603026094cde1378a
siyoon <ryuu.public@gmail.com>
2024-09-15 14:40:45 +0900

    chore: add basic settings

---
+++ src/main/java/com/neon/tonari/TonariApplication.java
@@ -0,0 +1,13 @@
+package com.neon.tonari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonariApplication.class, args);
+	}
+
+}
