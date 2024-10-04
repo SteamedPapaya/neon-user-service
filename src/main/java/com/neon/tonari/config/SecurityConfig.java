508c7b002ce86f5b300a077a7493b1b655eeaaea
siyoon <ryuu.public@gmail.com>
2024-10-04 21:38:53 +0900

    fix: remove /api prefix on path and form login

--- src/main/java/com/neon/tonari/config/SecurityConfig.java
+++ src/main/java/com/neon/tonari/config/SecurityConfig.java
@@ -48,8 +48,7 @@ public class SecurityConfig {
 
                 // 경로별 인증 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login**", "/css/**", "/js/**", "/api/auth/token", "/oauth2/**", "/login/oauth2/code/**", "/users/me").permitAll()  // 인증 불필요 경로
-                        .requestMatchers("/**").authenticated()  // JWT 인증이 필요한 경로
+                        .requestMatchers("/", "/login**", "/css/**", "/js/**", "/auth/token", "/oauth2/**", "/login/oauth2/code/**").permitAll()  // 인증 불필요 경로
                         .anyRequest().authenticated()  // 그 외 요청은 인증 필요
                 )
 
@@ -64,12 +63,6 @@ public class SecurityConfig {
                         })
                         .invalidateHttpSession(true)  // 세션 무효화
                         .clearAuthentication(true)    // 인증 정보 제거
-                )
-
-                // 폼 기반 로그인 설정
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .permitAll()
                 )
 
                 // OAuth2 로그인 설정
